0289998fec89c9f33265e753cb3ae3dfd1a8cf0a
Vincent0-8 <chenvincent8406@gmail.com>
2026-09-03 17:48:26 +0700

    Content Update

--- data/certifications.ts
+++ data/certifications.ts
@@ -4,22 +4,22 @@ export const certificationsList: CertificationItem[] = [
   {
     id: "artificial-intelligence",
     title: "Artificial Intelligence",
-    issuer: "Software Engineering Academy",
-    issueDate: "2026",
+    issuer: "Certiport Vue Pearson",
+    issueDate: "July 2026",
     imageUrl: "/images/certificates/Artificial Intelligence.webp",
   },
   {
     id: "agile-scrum-fundamental",
     title: "Agile Scrum Fundamental",
-    issuer: "Vocational Multimedia Institute",
-    issueDate: "2025",
+    issuer: "Mindmagine",
+    issueDate: "December 2025",
     imageUrl: "/images/certificates/Agile Scrum Fundamental.webp",
   },
   {
     id: "ef-set-certificate",
     title: "EF SET English Certificate",
-    issuer: "EF Standard English Test",
-    issueDate: "2025",
+    issuer: "EF SET",
+    issueDate: "July 2024",
     imageUrl: "/images/certificates/EF SET Certificate.webp",
   },
 ];
